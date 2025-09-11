fb8fd469750df4e0f7b8ee4ac138ad0b49f624cc
SirishaDuba <sirishaduba29@gmail.com>
2025-09-11 10:45:11 +0530

    Login page added

---
+++ src/Signup.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <input type='text' placeholder='Enter the Email'></input>
+                <br></br><br></br>
+        <input type='password' placeholder='Enter the Password'></input>
+                <br></br><br></br>
+        <input type='text' placeholder='Enter the Email'></input>
+                <br></br><br></br>
+        <input type='password' placeholder='Enter the Password'></input>
+                <br></br><br></br>
+      </div>
+    )
+  }
+}
